bcecbb46c1a308f31d72ee646b16fe6dd9de86a0
Nevena Ravic <nevenar.1995@gmail.com>
2026-02-05 21:52:18 +0100

    test

--- src/main/java/sandbox/KafkaSystemApplication.java
+++ src/main/java/sandbox/KafkaSystemApplication.java
@@ -1,13 +1,16 @@
 package sandbox;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class KafkaSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSystemApplication.class, args);
+		log.info("test");
 	}
 
 }
